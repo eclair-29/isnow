189d12c440874890293d6c85f2dc59976946b2ff
Miguel D <dmgzky@gmail.com>
2023-12-27 16:10:21 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -37,3 +33,32 @@ Route::group(['middleware' => ['role:requestor|approver|admin']], function () {
 Route::group(['middleware' => ['role:approver']], function () {
     Route::get('/approvals', 'ApproverController@index')->name('approvals.index');
 });
+
+// SeusRoutes(admin routes)
+Route::get('dashboard', function () {
+    return view('admin.dashboard');
+});
+/* 
+Route::get('/register', function () {
+    return view('admin.register');
+}); */
+
+Route::get('/forms', 'AccountController@index');
+
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+// Route::get('users', function () {
+//     return view('admin.users');
+// });
+
+// Route::get('approvers', function () {
+//     return view('admin.approvers');
+// });
+
+Route::get('/administrator', 'AdminController@index');
+Route::get('/users', 'UsersController@index');
+Route::get('/request', 'RequestsController@index');
+Route::get('/prices', 'AccountChangesController@index');
