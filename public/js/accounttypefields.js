675989dce80a3b6c3529e0ebabaa16face095ee7
Miguel D <dmgzky@gmail.com>
2024-01-08 16:58:12 +0800

    Add files via upload

--- public/js/accounttypefields.js
+++ public/js/accounttypefields.js
@@ -1,6 +1,7 @@
 const requestType = $("#request_type");
 const accountType = $("#account_type");
 
+const existingSapRoles = $("#existing_sap_roles");
 const sapRoles = $("#sap_roles");
 const existingSalesforce = $("#existing_salesforce_subtypes");
 const salesforce = $("#salesforce_subtypes");
@@ -8,12 +9,16 @@ const salesforce = $("#salesforce_subtypes");
 function showSubTypes() {
     switch (true) {
         case requestType.val() === "4" && accountType.val() === "3": // [New and SAP]
+            existingSapRoles.attr("hidden", true);
             salesforce.attr("hidden", true);
             existingSalesforce.attr("hidden", true);
             sapRoles.attr("hidden", false);
             break;
-        case requestType.val() === "6" && accountType.val() === "3": // [Delete and SAP]
-            //
+        case requestType.val() === "5" && accountType.val() === "3": // [Edit and SAP]
+            sapRoles.attr("hidden", false);
+            existingSapRoles.attr("hidden", false);
+            salesforce.attr("hidden", true);
+            existingSalesforce.attr("hidden", true);
             break;
         case requestType.val() === "4" && accountType.val() === "2": // [New and Salesforce]
             salesforce.attr("hidden", false);
@@ -23,8 +28,11 @@ function showSubTypes() {
         case requestType.val() === "5" && accountType.val() === "2": // [Edit and Salesforce]
             existingSalesforce.attr("hidden", false);
             salesforce.attr("hidden", false);
+            existingSapRoles.attr("hidden", true);
+            sapRoles.attr("hidden", true);
             break;
         default:
+            existingSapRoles.attr("hidden", true);
             sapRoles.attr("hidden", true);
             existingSalesforce.attr("hidden", true);
             salesforce.attr("hidden", true);
@@ -88,4 +96,22 @@ $("body").on("click", subTypeActionAdd, function () {
 $("body").on("click", subTypeActionDel, function () {
     $(this).closest(".col-6").remove();
     hideDelFieldBtn();
+});
+
+$("body").on("click", ".existing-subtype-action-btn", function () {
+    // console.log("input id", $(this).parent().next().attr("id"));
+    const inputId = $(this).parent().next().attr("id");
+    const subtypeForDelDiv = $(".subtype-for-del");
+
+    if (!$(this).hasClass("active")) {
+        subtypeForDelDiv.append(`
+            <input type="hidden" name="subtype_for_del[]" value="${inputId}" id="for_del_${inputId}">
+        `);
+    } else {
+        $(this)
+            .closest(`.col-6`)
+            .siblings(":last")
+            .find(`#for_del_${inputId}`)
+            .remove();
+    }
 });
