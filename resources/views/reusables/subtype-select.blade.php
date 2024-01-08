18bc1c991f716aa2f1a92843209dfe6e8d590017
Miguel D <dmgzky@gmail.com>
2024-01-08 17:05:38 +0800

    Add files via upload

--- resources/views/reusables/subtype-select.blade.php
+++ resources/views/reusables/subtype-select.blade.php
@@ -26,19 +26,23 @@
 </div>
 
 <div class="form-row" id="{{ $existingSubtypeId }}" hidden>
+    @unless (count($existingSubtypeData) == 0)
     <div class="col-12 pt-2">
         <p>Existing {{ $label }}s</p>
     </div>
+    @endunless
     @foreach ($existingSubtypeData as $value)
     <div class="col-6 form-group">
         <div class="input-group">
             <div class="input-group-prepend">
-                <button class="btn btn-outline-secondary subtype-action-btn" type="button"><i
+                <button data-toggle="button" aria-pressed="false"
+                    class="btn btn-outline-danger existing-subtype-action-btn" type="button"><i
                         data-feather="minus-circle"></i></button>
             </div>
             <input readonly type="text" class="form-control existing-subtype-field" id="{{ $value->id }}"
-                name="{{ $existingArr }}" value="{{ $value->description }}">
+                value="{{ $value->description }}">
         </div>
     </div>
     @endforeach
+    <div class="col-6 form-group subtype-for-del"></div>
 </div>
